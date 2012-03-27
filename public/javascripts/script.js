03bad5795945b888860ae91017a2eb60a7fc8a39
J2P <jjp5023@gmail.com>
2012-03-27 16:45:34 +0900

    게임오버 처리, 단어 삭제 모션 변경

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,8 +6,15 @@
 
 var Typing = Typing || {};
 
+/**
+ * @namespace Typing
+ * @class Init
+ * @require jQuery
+ *
+ * @author J2P
+ */
 Typing.Init = (function(tp, $){
-	var speed = 25000,
+	var speed = 10000,
 		level = 1,
 		time = null,
 		$container = $('#container'),
@@ -21,45 +28,8 @@ Typing.Init = (function(tp, $){
 		}());
 
 	/**
-	 * @method start
-	 */
-	var start = function() {
-		var $container = $('#container');
-
-		time = setInterval(function(){
-			var $newWord = tp.Word.createWord();
-
-			$container.append($newWord);
-			
-			tp.Word.startAnimate($newWord);
-		}, speed / 2);
-	};
-
-	/**
-	 * @method stop
-	 */
-	var stop = function() {
-		clearInterval(time);
-		time = null
-	};
-
-	/**
-	 * @method miss
-	 */
-	var miss = function() {
-		var bar = parseInt($('.bar')[0].style.width);
-
-		if (bar > 0) {
-			var out =  bar - 10;
-
-			$('.bar').animate({'width': out+'%'});
-		} else {
-			tp.Init.stop();
-		}
-	};
-
-	/**
 	 * @method setSpeed
+	 * @param {Number} sp
 	 */
 	var setSpeed = function(sp) {
 		speed = sp;
@@ -100,29 +70,77 @@ Typing.Init = (function(tp, $){
 		return width;
 	};
 
+	/**
+	 * @private
+	 */
+	var _resetProgress = function() {
+		var $progress = $('.bar');
+
+		$progress.animate({'width': '100%'}, 'fast');
+	};
+
+	/**
+	 * @method start
+	 */
+	var start = function() {
+		var $container = $('#container');
+
+		_resetProgress();
+
+		time = setInterval(function(){
+			var $newWord = tp.Word.create();
+
+			$container.append($newWord);
+			
+			tp.Word.startAnimate($newWord);
+		}, speed / 2);
+	};
+
+	/**
+	 * @method stop
+	 */
+	var stop = function() {
+		clearInterval(time);
+		time = null;
+
+		$('span.btn').stop().remove();
+
+		$('#closeModal').modal({'backdrop': 'static'});
+	};
+
+	/**
+	 * @method miss
+	 */
+	var miss = function() {
+		var bar = parseInt($('.bar')[0].style.width);
+		var out =  bar - 10;
+
+		$('.bar').animate({'width': out+'%'}, 'fast');
+
+		if (out == 0) {
+			tp.Init.stop();
+		}
+	};
+
+	$('#closeModal').on({
+		'click': function(event) {
+			event.preventDefault();
+			$('#closeModal').modal('hide');
+			tp.Init.start();
+		}
+	}, '.btn');
+
 	$('#wordBox').on({
 		'keypress': function(e) {
 			if (e.keyCode == '13') {
 				var $wordBox = $(this);
 				var $text = $wordBox.val();
-				var $words = $('.btn', $container);
-				var find = false;
 
-				$words.each(function(i){
-					if($(this).text() == $text) {
-						find = true;
-
-						$(this).stop().fadeOut(500, function(){
-							$(this).remove();
-						});
-					}
-				});
-
-				if (!find) {
-					tp.Init.miss();
+				if ($wordBox.val() == '') {
+					return false;
 				}
 
-				$wordBox.val('');
+				tp.Word.find($wordBox, $text);
 
 				return false;
 			}
@@ -145,10 +163,17 @@ Typing.Init = (function(tp, $){
 	}
 }(Typing, jQuery));
 
+/**
+ * @namespace Typing
+ * @class Word
+ * @require jQuery
+ *
+ * @author J2P
+ */
 
 Typing.Word = (function(tp, $){
 	var words = [
-			'리오', 
+			'리오',
 			'아웃사이더', 
 			'빡써', 
 			'에이제이', 
@@ -176,14 +201,15 @@ Typing.Word = (function(tp, $){
 			'대니', 
 			'팔루트', 
 			'나나파크'
-		];
+		],
+		$container = $('#container');
 
 	/**
 	 * @method createStyle
 	 */
-	var createStyle = function() {
+	var style = function() {
 		var random = Math.floor(Math.random() * 7),
-			wordStyle = ['', '-primary', '-info', '-success', '-warning', '-danger', '-inverse'];
+			wordStyle = ['', '-info', '-success', '-warning', '-danger', '-inverse'];
 
 		return wordStyle[random];
 	};
@@ -191,7 +217,7 @@ Typing.Word = (function(tp, $){
 	/**
 	 * @method createWord
 	 */
-	var createWord = function() {
+	var create = function() {
 		var $container = $('#container'),
 			$left = tp.Init.getLeft(),
 			$width = tp.Init.getWidth(),
@@ -199,7 +225,7 @@ Typing.Word = (function(tp, $){
 
 			$newWord = $('<span>', {
 				text: words[Math.floor(Math.random() * words.length)],
-				class: 'btn btn'+this.createStyle()
+				class: 'btn btn'+this.style()
 			}).css({
 				'position': 'absolute',
 				'top': '0',
@@ -211,6 +237,7 @@ Typing.Word = (function(tp, $){
 
 	/**
 	 * @method startAnimate
+	 * @param {Elment} $newWord new span Element
 	 */
 	var startAnimate = function($newWord) {
 		$newWord.animate({
@@ -223,14 +250,52 @@ Typing.Word = (function(tp, $){
 		});
 	};
 
-	var stopAnimate = function() {
+	var find = function($wordBox, $text) {
+		var $words = $('.btn', $container);
+		var find = false;
 
+		$words.each(function(i){
+			if($(this).text() == $text) {
+				find = true;
+
+				/*$(this).stop().fadeOut(500, function(){
+					$(this).remove();
+				});*/
+
+				var $btn = $(this);
+				var $style = 'badge-'+$btn.attr('class').replace(/btn-?\s?/g, '').replace('danger', 'error');
+
+				$('span.badge').addClass($style).fadeIn('fast', function(){
+					$btn.stop();
+				}).animate({
+					'left' : parseInt($(this).css('left'), 10) + ($btn.width() / 2),
+					'top': parseInt($(this).css('top'), 10) + $btn.height()
+				}, function() {
+					$btn.fadeOut(500, function(){
+						$(this).remove();
+					});
+
+					$(this).fadeOut(200, function(){
+						$(this).css({
+							'left': '480px',
+							'top': '680px'
+						}).removeClass($style);
+					});
+				});
+			}
+		});
+
+		if (!find) {
+			tp.Init.miss();
+		}
+
+		$wordBox.val('');
 	};
 
 	return {
-		createStyle: createStyle,
-		createWord: createWord,
+		style: style,
+		create: create,
 		startAnimate: startAnimate,
-		stopAnimate: stopAnimate
+		find: find
 	}
 }(Typing, jQuery));

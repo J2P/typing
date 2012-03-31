5080ec5b6f822f4a5f4d830dd9a52ee6032d016c
J2P <jjp5023@gmail.com>
2012-03-31 12:57:34 +0900

    점수기능 & 레벨 기능 구현

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -43,6 +43,20 @@ Typing.Init = (function(tp, $){
 	};
 
 	/**
+	 * @method setLevel
+	 */
+	var setLevel = function() {
+		++level;
+	}
+
+	/**
+	 * @method getLevel
+	 */
+	var getLevel = function() {
+		return level;
+	}
+
+	/**
 	 * @private
 	 */
 	var _setLeft = function() {
@@ -82,15 +96,36 @@ Typing.Init = (function(tp, $){
 	/**
 	 * @private
 	 */
+	 var _resetLevel = function() {
+	 	var $level = $('#level');
+
+	 	level = 1;
+	 	$level.text(1);
+	 }
+
+	 /**
+	  * @private 
+	  */
+	 var _resetScore = function() {
+	 	var $score = $('#score');
+
+	 	$score.text(0);
+	 }
+
+
+	/**
+	 * @private
+	 */
 	var _startGame = function(event) {
 		event.preventDefault();
 
 		var $modalBackdrop = $('.modal-backdrop');
-		var $container = $('#container');
 
 		$(this).parents('.modal').modal('hide');
 
 		_resetProgress();
+		_resetLevel();
+		_resetScore();
 
 		time = setInterval(function(){
 			var $newWord = tp.Word.create();
@@ -112,6 +147,19 @@ Typing.Init = (function(tp, $){
 
 		$('#closeModal').modal({'backdrop': 'static'});
 	};
+
+	var restart = function() {
+		clearInterval(time);
+		time = null;
+
+		time = setInterval(function(){
+			var $newWord = tp.Word.create();
+
+			$container.append($newWord);
+			
+			tp.Word.startAnimate($newWord);
+		}, speed / 2);
+	}
 
 	/**
 	 * @method miss
@@ -164,13 +212,14 @@ Typing.Init = (function(tp, $){
 		_setWidth();
 	});
 
-	
-
 	return {
 		stop: stop,
+		restart: restart,
 		miss: miss,
 		getSpeed: getSpeed,
 		setSpeed: setSpeed,
+		getLevel: getLevel,
+		setLevel: setLevel,
 		getLeft: getLeft,
 		getWidth: getWidth
 	}
@@ -185,37 +234,35 @@ Typing.Init = (function(tp, $){
  */
 
 Typing.Word = (function(tp, $){
-	var words = [
-			'리오',
-			'아웃사이더', 
-			'빡써', 
-			'에이제이', 
-			'브리즈', 
-			'찌우니', 
-			'오디스', 
-			'파이어준', 
-			'욱사마', 
-			'제이투피', 
-			'보이', 
-			'대니', 
-			'팔루트', 
-			'나나파크',
-			'리오', 
-			'아웃사이더', 
-			'빡써', 
-			'에이제이', 
-			'브리즈', 
-			'찌우니', 
-			'오디스', 
-			'파이어준', 
-			'욱사마', 
-			'제이투피', 
-			'보이', 
-			'대니', 
-			'팔루트', 
-			'나나파크'
-		],
-		$container = $('#container');
+	var words = 'FRENDS 가 구성된지 언 2년이 되어갑니다. 그동안 발표 형식의 기술 공유를 많이 해왔었고 올해는 서로 모여서 코딩하는 모임을 갖어보고자 시작하였습니다. 그리고 작은 아이디어도 함께 모여 구현을 해보는 그런 행사의 첫 시발점이 되고자 내부의 행사를 마련해서 진행을 해보고 있습니다.오픈 커뮤니티가 아니고 행사 운영에 대한 경험도 없고 시행착오에 대한 리스크를 떠 않고 행사를 키우기에는 부담이 있어 FRENDS 내부적으로 행사를 진행하고 있습니다.내부적인 행사이기는 하지만 해카톤 진행에서 나온 결과는 최대한 오픈할 계획입니다.'.split(' '),
+		$container = $('#container'),
+		myscore = 0;
+
+	/**
+	 * @private
+	 * @param {Number}
+	 * @return {Number} myscore
+	 */
+	var _updateScore = function(sc) {
+		myscore += 10 - sc;
+
+		return myscore;
+	};
+
+	/**
+	 * @private
+	 * @param {Number} level
+	 */
+	var _changeLevel = function(level) {
+		var getLevel = tp.Init.getLevel;
+		var getSpeed = tp.Init.getSpeed;
+
+		tp.Init.setLevel();
+
+		$('#level').text(getLevel());
+
+		tp.Init.setSpeed(getSpeed() - getLevel() * 500);
+	};
 
 	/**
 	 * @method createStyle
@@ -256,24 +303,27 @@ Typing.Word = (function(tp, $){
 		$newWord.animate({
 			'top': '+=672'
 		}, tp.Init.getSpeed(), function(){
-			$newWord.fadeOut(1000, function(){
+			$newWord.fadeOut(200, function(){
 				$(this).remove();
 				tp.Init.miss();
 			});
 		});
 	};
 
+	/**
+	 * @method find
+	 * @param {Element} $wordBox 
+	 * @param {String} $text
+	 */
 	var find = function($wordBox, $text) {
-		var $words = $('.btn', $container);
+		var $words = $('.btn', $container),
+			$score = $('#score');
+
 		var find = false;
 
 		$words.each(function(i){
 			if($(this).text() == $text) {
 				find = true;
-
-				/*$(this).stop().fadeOut(500, function(){
-					$(this).remove();
-				});*/
 
 				var $btn = $(this);
 				var $style = 'badge-'+$btn.attr('class').replace(/btn-?\s?/g, '').replace('danger', 'error');
@@ -282,10 +332,31 @@ Typing.Word = (function(tp, $){
 					$btn.stop();
 				}).animate({
 					'left' : parseInt($(this).css('left'), 10) + ($btn.width() / 2),
-					'top': parseInt($(this).css('top'), 10) + $btn.height()
+					'top': parseInt($(this).css('top'), 10) + $btn.height() + (tp.Init.getLevel() * 3)
 				}, function() {
 					$btn.fadeOut(500, function(){
+						var $topValue = Math.floor(parseInt($(this).css('top'), 10)/70);
+						var level  = tp.Init.getLevel();
+
 						$(this).remove();
+
+						$score.text(_updateScore($topValue));
+
+						if (myscore >= 100 && level === 1) {
+							_changeLevel();
+							tp.Init.restart();
+						} else if (myscore >= 200 && level === 2) {
+							_changeLevel();
+							tp.Init.restart();
+						} else if (myscore >= 300 && level === 3) {
+							_changeLevel();
+							tp.Init.restart();
+						} else if (myscore >= 400 && level === 4) {
+							_changeLevel();
+							tp.Init.restart();
+						} else if (myscore >= 500 && level === 5) {
+							tp.Init.stop();	
+						}
 					});
 
 					$(this).fadeOut(200, function(){

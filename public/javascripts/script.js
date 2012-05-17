6238223b32c08f42cbfc3f9bf916a22133ab6ed4
J2P <jjp5023@gmail.com>
2012-05-18 01:50:16 +0900

    게임방식 변경
    기존 소나기 게임 방식에서 다른 방식으로 변경

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,17 +16,11 @@ var Typing = Typing || {};
 Typing.Init = (function(tp, $){
 	var speed = 10000,
 		level = 1,
-		time = null,
 		myscore = 0,
+		time = null,
+		timer = null,
 		$container = $('#container'),
-
-		left = (function(){
-			return $container.offset().left + 20;
-		}()),
-
-		width = (function(){
-			return $container.width() - 100;
-		}());
+		$wordWrap = $('.wordWrap');
 
 	/**
 	 * @method setSpeed
@@ -59,10 +53,12 @@ Typing.Init = (function(tp, $){
 
 	/**
 	 * @method setScore
-	 * @param {Number} sc
+	 * @param {Number} point
 	 */
-	var setScore = function(sc) {
-		myscore += 10 - sc;
+	var setScore = function(point) {
+		myscore += parseInt(point);
+
+		_changeScore();
 	};
 
 	/**
@@ -72,33 +68,11 @@ Typing.Init = (function(tp, $){
 		return myscore;
 	}
 
-	/**
-	 * @private
-	 */
-	var _setLeft = function() {
-		left = $container.offset().left + 20;
+	var _changeScore = function() {
+		var $score = $('#score');
+
+		$score.text(getScore());
 	}
-
-	/**
-	 * @method getLeft
-	 */
-	var getLeft = function() {
-		return left;
-	};
-
-	/**
-	 * @private
-	 */
-	var _setWidth = function() {
-		width = $container.width() - 100;
-	}
-
-	/**
-	 * @method getWidth
-	 */
-	var getWidth = function() {
-		return width;
-	};
 
 	/**
 	 * @private
@@ -106,7 +80,8 @@ Typing.Init = (function(tp, $){
 	var _resetProgress = function() {
 		var $progress = $('.bar');
 
-		$progress.animate({'width': '100%'}, 'fast');
+		$progress.text('100').css('width', '100%');
+		_startTimer();
 	};
 
 	/**
@@ -119,15 +94,37 @@ Typing.Init = (function(tp, $){
 	 	$level.text(1);
 	 }
 
-	 /**
-	  * @private 
-	  */
-	 var _resetScore = function() {
-	 	var $score = $('#score');
+	/**
+	 * @private 
+	 */
+	var _resetScore = function() {
+		var $score = $('#score');
 
 	 	$score.text(0);
-	 }
+	};
 
+	/**
+	 * @private
+	 */
+	var _startTimer = function() {
+		var $width = 100;
+		var $text = 100;
+		timer = setInterval(function() {
+			var $progress = $('.bar');
+
+			if ($text > 0) {
+				$progress.text(--$text);
+			} 
+
+			if ($width > 0) {
+				$progress.animate({'width': --$width+'%'}, 'fast');
+			}
+
+			if ($text === 0 && $width === 0) {
+				tp.Init.stop();
+			}
+		}, 1000);
+	}
 
 	/**
 	 * @private
@@ -143,105 +140,84 @@ Typing.Init = (function(tp, $){
 		_resetLevel();
 		_resetScore();
 
-		time = setInterval(function(){
-			var $newWord = tp.Word.create();
+		tp.Word.init();
 
-			$container.append($newWord);
-			
-			tp.Word.startAnimate($newWord);
-		}, speed / 2);
+		if (time == null) {
+			time = setInterval(function(){
+				tp.Word.add(function(){
+					if ($('.wordWrap').height() >= 700) {
+						tp.Init.stop();
+					}
+				});
+			}, speed/level);
+		}
+	};
+
+	/**
+	 * @private
+	 */
+	var _enter = function(event) {
+		if (event.keyCode == '13') {
+			var $wordBox = $(this);
+			var $text = $wordBox.val();
+
+			if ($wordBox.val() == '') {
+				return false;
+			}
+
+			tp.Word.find($wordBox, $text);
+
+			return false;
+		}
 	};
 
 	/**
 	 * @method stop
 	 */
 	var stop = function() {
+		var $wordBox = $('#wordBox');
 		clearInterval(time);
+		clearInterval(timer);
 		time = null;
+		timer = null;
 
-		$('span.btn').stop().remove();
+		$wordBox.blur();
 
-		$('#levelView').text(level + '레벨');
 		$('#scoreView').text(myscore + '점');
 		$('#closeModal').modal({'backdrop': 'static'});
 	};
 
+	/**
+	 * @method restart
+	 */
 	var restart = function() {
 		clearInterval(time);
 		time = null;
 
-		time = setInterval(function(){
-			var $newWord = tp.Word.create();
-
-			$container.append($newWord);
-			
-			tp.Word.startAnimate($newWord);
-		}, speed / 2);
-	}
-
-	/**
-	 * @method miss
-	 */
-	var miss = function() {
-		var bar = parseInt($('.bar')[0].style.width);
-		var out =  bar - 10;
-
-		$('.bar').animate({'width': out+'%'}, 'fast');
-
-		if (out == 0) {
-			tp.Init.stop();
-		}
+		tp.Word.init();
 	};
 
+	//처음 시작전 모달창
 	$('#startModal').modal({'backdrop': 'static'});
 
-	/**
-	 * 다시시작
-	 */
+	//다시시작
 	$('#closeModal').on({'click': _startGame}, '.btn');
 
-	/**
-	 * 처음시작
-	 */
+	//처음시작
 	$('#startModal').on({'click': _startGame}, '.btn-primary');
 
-	/**
-	 * 입력상자
-	 */
-	$('#wordBox').on({
-		'keypress': function(e) {
-			if (e.keyCode == '13') {
-				var $wordBox = $(this);
-				var $text = $wordBox.val();
-
-				if ($wordBox.val() == '') {
-					return false;
-				}
-
-				tp.Word.find($wordBox, $text);
-
-				return false;
-			}
-		}
-	});
-
-	$(window).on('resize', function(){
-		_setLeft();
-		_setWidth();
-	});
+	//입력상자
+	$('#wordBox').on({'keypress': _enter});
 
 	return {
 		stop: stop,
 		restart: restart,
-		miss: miss,
 		getSpeed: getSpeed,
 		setSpeed: setSpeed,
 		getLevel: getLevel,
 		setLevel: setLevel,
 		getScore: getScore,
-		setScore: setScore,
-		getLeft: getLeft,
-		getWidth: getWidth
+		setScore: setScore
 	}
 }(Typing, jQuery));
 
@@ -254,10 +230,10 @@ Typing.Init = (function(tp, $){
  */
 
 Typing.Word = (function(tp, $){
-	var words = 'FRENDS 가 구성된지 언 2년이 되어갑니다. 그동안 발표 형식의 기술 공유를 많이 해왔었고 올해는 서로 모여서 코딩하는 모임을 갖어보고자 시작하였습니다. 그리고 작은 아이디어도 함께 모여 구현을 해보는 그런 행사의 첫 시발점이 되고자 내부의 행사를 마련해서 진행을 해보고 있습니다.오픈 커뮤니티가 아니고 행사 운영에 대한 경험도 없고 시행착오에 대한 리스크를 떠 않고 행사를 키우기에는 부담이 있어 FRENDS 내부적으로 행사를 진행하고 있습니다.내부적인 행사이기는 하지만 해카톤 진행에서 나온 결과는 최대한 오픈할 계획입니다.'.split(' '),
+	var words = window.words = ['constructor', 'hasOwnProperty', 'isPrototypeof', 'propertyIsEnumerable', 'toLocaleString', 'toString', 'valueOf', 'create', 'defineProperty', 'defineProperties', 'getOwnPropertyDescriptor', 'keys', 'getOwnPropertyNames', 'getPrototypeOf', 'preventExtensions', 'isExtensible', 'seal', 'isSealed', 'freeze', 'isFrozen', 'apply', 'bind', 'call', 'isGenerator', 'toString', 'isArray', 'pop', 'push', 'reverse', 'shift', 'sort', 'splice', 'unshift', 'concat', 'join', 'slice', 'indexOf', 'lastIndexOf', 'filter', 'forEach', 'every', 'map', 'some', 'reduce', 'reduceRight', 'charAt', 'charCodeAt', 'concat', 'indexof', 'lastIndexOf', 'localeCompare', 'match', 'replace', 'search', 'slice', 'split', 'substr', 'substring', 'toLocaleLowerCase', 'toLocaleUpperCase', 'toLowerCase', 'toString', 'toUpperCase', 'trim', 'trimLeft', 'trimRight', 'valueof', 'exec', 'test', 'getDate', 'getDay', 'getFullYear', 'getHours', 'getmilliseconds', 'getMinutes', 'getMonth', 'getSeconds', 'getTime', 'getTimezoneOffset', 'getUTCDate', 'getUTCDay', 'getUTCFullYear', 'getUTCHours', 'getUTCMilliseconds', 'getUTCMinutes', 'getUTCMonth', 'getUTCSeconds', 'setDate', 'setFullYear', 'getHours', 'setMilliseconds', 'setMinutes', 'setMonth', 'setSeconds', 'setTime', 'setUTCDate', 'setUTCFullYear', 'setUTCHours', 'setUTCMiliseconds', 'setUTCMinutes', 'setUTCMonth', 'setUTCSeconds', 'toDateString', 'toISOString', 'toJSON', 'toLocaleDatString', 'toLocaleString', 'toLocaleTimeString', 'toTimeString', 'toUTCString'],
+
 		$container = $('#container'),
-		myscore = 0;
-		
+		$wordWrap = $('.wordWrap');		
 
 	/**
 	 * @private
@@ -275,49 +251,83 @@ Typing.Word = (function(tp, $){
 	};
 
 	/**
+	 * @method init
+	 */
+	var init = function() {
+		var len = parseInt(words.length / 7) + 1;
+		
+		$wordWrap.empty();
+
+		for (var i = 0; i < 6; i++) {
+			var rnd = (function(){
+				return Math.floor( (Math.random() * ((words.length - 7) - 0 + 1)) + 0 );
+			}());
+			var sliceWord = words.slice(rnd, parseInt(rnd+7));
+
+			$wordWrap.append('<p>');
+
+			$.each(sliceWord, function(e) {
+				$('p', $wordWrap).last().append(_create(sliceWord[e]));
+			});
+		}
+
+		//순서대로 뿌려주는 방식.
+		/*for (var i = 0; i < len; i++) {
+			var sliceWord = words.slice(i*7, parseInt((i*7) + 7));
+			$wordWrap.append('<p>');
+
+			$.each(sliceWord, function(e) {
+				$('p', $wordWrap).last().append(_create(sliceWord[e]));
+			});
+		}*/
+		
+	};
+
+	/**
+	 * @method add
+	 * @param {Function} callback
+	 */
+	var add = function(callback) {
+		var rnd = Math.floor( (Math.random() * ((words.length - 7) - 0 + 1)) + 0 );
+		var sliceWord = words.slice(rnd, parseInt(rnd+7));
+
+		$('p', $wordWrap).eq(0).before('<p>');
+
+		for (var i = 0; i < 7; i++) {
+			$('p',$wordWrap).eq(0).append(_create(sliceWord[i]));
+		}
+
+		callback();
+	}
+
+	/**
 	 * @method createStyle
 	 */
 	var style = function() {
 		var random = Math.floor(Math.random() * 7),
 			wordStyle = ['', '-info', '-success', '-warning', '-danger', '-inverse'];
 
-		return wordStyle[random];
+		return {
+			word: wordStyle[random],
+			point: random + 1
+		}
 	};
 
 	/**
 	 * @method createWord
+	 * @param {String} $word
 	 */
-	var create = function() {
-		var $container = $('#container'),
-			$left = tp.Init.getLeft(),
-			$width = tp.Init.getWidth(),
-			$position = Math.floor( (Math.random() * ($width - $left)) + $left ),
+	var _create = function($word) {
+		var style = tp.Word.style();
 
-			$newWord = $('<span>', {
-				'text': words[Math.floor(Math.random() * words.length)],
-				'class': 'btn btn'+this.style()
-			}).css({
-				'position': 'absolute',
-				'top': '0',
-				'left': $position
-			});
+		$newWord = $('<span>', {
+			'text': $word,
+			'class': 'btn btn'+style.word,
+			'data-id': $word,
+			'data-point': style.point 
+		});
 
 		return $newWord
-	};
-
-	/**
-	 * @method startAnimate
-	 * @param {Elment} $newWord new span Element
-	 */
-	var startAnimate = function($newWord) {
-		$newWord.animate({
-			'top': '+=672'
-		}, tp.Init.getSpeed(), function(){
-			$newWord.fadeOut(200, function(){
-				$(this).remove();
-				tp.Init.miss();
-			});
-		});
 	};
 
 	/**
@@ -326,72 +336,36 @@ Typing.Word = (function(tp, $){
 	 * @param {String} $text
 	 */
 	var find = function($wordBox, $text) {
-		var $words = $('.btn', $container),
-			$score = $('#score');
+		var $word = $('.btn[data-id="'+$text+'"]', $container);	
 
-		var find = false;
+		if ($word.length > 0) {
+			var $height = $word.height(),
+				$width = parseInt($word.width()/2),
+				$style = 'badge-'+$word.attr('class').replace(/btn-?\s?/g, '').replace('danger', 'error');
 
-		$words.each(function(i){
-			if($(this).text() == $text) {
-				find = true;
 
-				var $btn = $(this);
-				var $style = 'badge-'+$btn.attr('class').replace(/btn-?\s?/g, '').replace('danger', 'error');
+			//@TODO
+			/*$container.append($('<span>', {
+				'class': 'badge '+$style
+			}).animate({
+				top: $top,
+				left: $left
+			}, 'fast',function(){
 
-				$('#container .badge').addClass($style).fadeIn('fast', function(){
-					$btn.stop();
-				}).animate({
-					'left' : parseInt($(this).css('left'), 10) + ($btn.width() / 2),
-					'top': parseInt($(this).css('top'), 10) + $btn.height() + (tp.Init.getLevel() * 3)
-				}, function() {
-					$btn.fadeOut(500, function(){
-						var $topValue = Math.floor(parseInt($(this).css('top'), 10)/70);
-						var level  = tp.Init.getLevel();
+			}));*/
 
-						$(this).remove();
-						tp.Init.setScore($topValue);
-						myscore = tp.Init.getScore();
-						$score.text(myscore);
-						
+			tp.Init.setScore($word.attr('data-point'));
 
-						if (myscore >= 100 && level === 1) {
-							_changeLevel();
-							tp.Init.restart();
-						} else if (myscore >= 200 && level === 2) {
-							_changeLevel();
-							tp.Init.restart();
-						} else if (myscore >= 300 && level === 3) {
-							_changeLevel();
-							tp.Init.restart();
-						} else if (myscore >= 400 && level === 4) {
-							_changeLevel();
-							tp.Init.restart();
-						} else if (myscore >= 500 && level === 5) {
-							tp.Init.stop();	
-						}
-					});
-
-					$(this).fadeOut(200, function(){
-						$(this).css({
-							'left': '480px',
-							'top': '680px'
-						}).removeClass($style);
-					});
-				});
-			}
-		});
-
-		if (!find) {
-			tp.Init.miss();
-		}
+			$word.remove();
+		} 
 
 		$wordBox.val('');
 	};
 
 	return {
+		init: init,
+		add: add,
 		style: style,
-		create: create,
-		startAnimate: startAnimate,
 		find: find
 	}
 }(Typing, jQuery));

514453ebd570e849f8b78a83fbb5c5dfc6ed435a
J2P <jjp5023@gmail.com>
2013-10-01 15:15:15 +0900

    파일이름 변경 & 포트변경

--- typing.js
+++ typing.js
@@ -84,7 +84,7 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRe
 app.get('/', routes.index);
 app.get('/join', routes.join);
 
-app.listen(3000, function() {
+app.listen(5029, function() {
 	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
